d253bd1a10fd499601040b470e0812bee57660b3
piratemermaid <schnur.ariel@gmail.com>
2022-09-25 16:25:44 -0600

    Use sx prop

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,34 +1,25 @@
 import { useSelector } from "react-redux";
-import clsx from "clsx";
-import { Card as CardMUI, CardContent, Typography } from "@mui/material";
-import { makeStyles } from "@mui/styles";
+import { Box, Card as CardMUI, CardContent, Typography } from "@mui/material";
 
-// card content styles for when we switch to MUI
-const useStyles = makeStyles({
-    card: { padding: "0 30px !important" },
-    cardTitle: { paddingBottom: 0 },
-    nightModeCard: {
-        background: "#9e9e9e !important",
-        color: "#e0e0e0 !important"
-    }
-});
+const nightModeCardSx = {
+    background: "#9e9e9e !important",
+    color: "#e0e0e0 !important"
+};
 
 const Card = ({ title, children }) => {
     const { nightMode } = useSelector((state) => state.appState);
 
-    const classes = useStyles();
-
     return (
         <CardMUI
             variant="outlined"
-            className={clsx(
-                classes.card,
-                nightMode ? classes.nightModeCard : ""
-            )}
+            sx={{
+                ...(nightMode ? nightModeCardSx : {}),
+                padding: "0 30px !important"
+            }}
         >
-            <div className={classes.cardTitle}>
+            <Box sx={{ paddingBottom: 0 }}>
                 <h3 style={{ marginBottom: 0 }}>{title}</h3>
-            </div>
+            </Box>
             <CardContent>{children}</CardContent>
         </CardMUI>
     );
--- client/src/components/SiteWarning.js
+++ client/src/components/SiteWarning.js
@@ -1,15 +1,12 @@
 import { makeStyles } from "@mui/styles";
 import { Box } from "@mui/material";
 
-const useStyles = makeStyles({
-    warning: { textAlign: "center", padding: "10px 0" }
-});
-
 const SiteWarning = () => {
-    const classes = useStyles();
-
     return (
-        <Box className={`${classes.warning} orange darken-1}`}>
+        <Box
+            className="orange darken-1"
+            sx={{ textAlign: "center", padding: "10px 0" }}
+        >
             WARNING: The backend is down, which means nothing is working. I am
             working on it.
         </Box>
